cecac5c276fdb5953aae52d60ba72fc4fa606806
aarora16 <a.99.anshul@gmail.com>
2020-12-01 14:42:36 -0600

    rendered pictures

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+document.addEventListener('DOMContentLoaded', () => {
+  fetchPictureData();
+});
+
+const fetchPictureData = () => {
+  fetch('http://localhost:3000/pictures')
+    .then(res => res.json())
+    .then(data => renderPictureData(data))
+};
+
+const renderPictureData = (pictures) => {
+  const testDiv = document.getElementById('test')
+  console.log(testDiv)
+  pictures.forEach(picture => {
+    let imgContainer = document.createElement('div')
+    let img = document.createElement('img')
+    img.src = picture['img_url']
+
+    testDiv.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+  });
+};
