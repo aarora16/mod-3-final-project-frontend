bc5de005cbca15392ef497faa098673e9e8c6ccc
aarora16 <a.99.anshul@gmail.com>
2020-12-08 09:05:36 -0600

    final product

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // fetchPictureData();
   hideForm();
   loginButtonListener();
   submitFormListener();
@@ -53,9 +52,23 @@ const postUserToAPI = (form) => {
   const lastName = form.querySelector('#last-name-input').value;
   const username = form.querySelector('#username-input').value;
 
-  fetch('http://localhost:3000/spotters')
+  if (firstName && lastName && username) {
+    fetch('http://localhost:3000/spotters'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        username: username,
+      })
+    }
+  } else {
+    fetch('http://localhost:3000/spotters')
     .then(res => res.json())
     .then(data => verifyUsername(data, username))
+  }
 };
 
 const verifyUsername = (data, username) => {
@@ -69,36 +82,128 @@ const verifyUsername = (data, username) => {
   });
 
   if (validUser != '') {
-    renderUserPictures(validUser);
+    displayUserInfo(validUser);
   } else {
     errorNotice.innerText = "Sorry that isn't quite right."
   };
 };
 
-const renderUserPictures = (userId) => {
-  document.querySelector('.login-container').innerHTML = '';
+const displayUserInfo = (userId) => {
+  fetch(`http://localhost:3000/spotters`)
+    .then(res => res.json())
+    .then(data => submitUserData(data, userId));
 };
 
+const submitUserData = (data, userId) => {
+  data.forEach(spotter => {
+    if (spotter.id == userId) {
+      renderUserData(spotter);
+    };
+  });
+};
+
+const renderUserData = (userData) => {
+  fetch('http://localhost:3000/pictures')
+    .then(res => res.json())
+    .then(data => renderUserPictures(data, userData))
+};
+
+const renderUserPictures = (pictureData, userData) => {
+  const body = document.querySelector('body');
+  body.innerHTML = '';
+  createUserHeading(userData);
+  const pictureDiv = document.createElement('div');
+  pictureDiv.classList.add('pictures');
+  body.appendChild(pictureDiv);
 
 
+  pictureData.forEach(picture => {
+    if (picture['spotter_id'] == userData['id']) {
+      createPictureElement(picture);
+    };
+  });
+  createPictureButton();
+};
 
-// const fetchPictureData = () => {
-//   fetch('http://localhost:3000/pictures')
-//     .then(res => res.json())
-//     .then(data => renderPictureData(data))
-// };
+const createImageGallery = () => {
+  fetch('http://localhost:3000/pictures')
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(picture => {
+        createPictureElement(picture);
+      })
+    })
+};
 
-// const renderPictureData = (pictures) => {
-//   const testDiv = document.querySelector('body');
-//   pictures.forEach(picture => {
-//     let imgContainer = document.createElement('div');
-//     let img = document.createElement('img');
+const createUserHeading = (userData) => {
+  const body = document.querySelector('body');
+  const headingContainer = document.createElement('div');
+  const heading = document.createElement('h1');
+  const yourGallery = document.createElement('h3');
 
-//     img.src = picture['img_url'];
-//     img.classList.add('plane-picture');
-//     imgContainer.classList.add('picture-container');
+  yourGallery.innerText = 'Your Gallery';
+  heading.innerText = `Welcome Back ${userData['first_name']}`;
+  headingContainer.classList.add('user-welcome');
 
-//     testDiv.appendChild(imgContainer);
-//     imgContainer.appendChild(img);
-//   });
-// };
+  headingContainer.appendChild(heading);
+  headingContainer.appendChild(yourGallery);
+  body.appendChild(headingContainer);
+};
+
+const createPictureElement = (picture) => {
+  const pictureDiv = document.querySelector('.pictures');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  const desc = document.createElement('div');
+  const a = document.createElement('a');
+
+  a.target = '_blank';
+  a.href = picture['img_url'];
+  imgContainer.classList.add('gallery');
+  img.src = picture['img_url'];
+  img.width = '600';
+  img.height = '400';
+  desc.innerText = picture.description;
+  desc.classList.add('desc');
+  
+  a.appendChild(img);
+  imgContainer.appendChild(a);
+  imgContainer.appendChild(desc);
+  pictureDiv.appendChild(imgContainer);
+};
+
+const createPictureButton = () => {
+  const pictures = document.querySelector('.pictures');
+  const allPicturesButton = document.createElement('button');
+  const backButton = document.createElement('button');
+  const buttonDiv = document.createElement('div');
+
+  buttonDiv.classList.add('buttons');
+  allPicturesButton.classList.add('action-button', 'other-button');
+  backButton.classList.add('action-button', 'other-button');
+
+  allPicturesButton.innerText = 'All Pictures';
+  backButton.innerText = 'Back';
+
+  buttonDiv.appendChild(allPicturesButton);
+  buttonDiv.appendChild(backButton);
+  pictures.appendChild(buttonDiv);
+
+  allPicturesListener(allPicturesButton);
+  backButtonListener(backButton);
+};
+
+const allPicturesListener = (button) => {
+  button.addEventListener('click', () => {
+    document.querySelector('.pictures').innerHTML = '';
+    document.querySelector('h3').innerText = 'All Pictures';
+    createImageGallery();
+    createPictureButton();
+  });
+};
+
+const backButtonListener = (button) => {
+  button.addEventListener('click', () => {
+    location.reload();
+  });
+};
